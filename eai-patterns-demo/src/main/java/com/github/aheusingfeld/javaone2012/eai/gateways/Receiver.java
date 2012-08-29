3be6843dbd033795a0c5f7dcd72d9476de130473
Alexander Heusingfeld <aheusingfeld@firstpoint.de>
2012-08-30 00:29:27 +0200

    Version 1.1 of the slides including review refactorings

---
+++ eai-patterns-demo/src/main/java/com/github/aheusingfeld/javaone2012/eai/gateways/Receiver.java
@@ -0,0 +1,9 @@
+package com.github.aheusingfeld.javaone2012.eai.gateways;
+
+/**
+ * Created with IntelliJ IDEA. User: ahe Date: 26.08.12 Time: 23:02 To change this template use File | Settings | File
+ * Templates.
+ */
+public interface Receiver
+{
+}
---
+++ eai-patterns-demo/src/main/java/com/github/aheusingfeld/javaone2012/eai/servlets/SimpleServlet.java
@@ -0,0 +1,9 @@
+package com.github.aheusingfeld.javaone2012.eai.servlets;
+
+/**
+ * Created with IntelliJ IDEA. User: ahe Date: 27.08.12 Time: 00:52 To change this template use File | Settings | File
+ * Templates.
+ */
+public class SimpleServlet
+{
+}
