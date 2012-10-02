0ad7e3b8df9c1836aad5e2a5ea2276b0e9825f81
Alexander Heusingfeld <aheusingfeld@firstpoint.de>
2012-10-02 09:41:44 -0700

    clean up

--- eai-patterns-demo/src/main/java/com/github/aheusingfeld/javaone2012/eai/servlets/SimpleServlet.java
+++
@@ -1,9 +0,0 @@
-package com.github.aheusingfeld.javaone2012.eai.servlets;
-
-/**
- * Created with IntelliJ IDEA. User: ahe Date: 27.08.12 Time: 00:52 To change this template use File | Settings | File
- * Templates.
- */
-public class SimpleServlet
-{
-}
